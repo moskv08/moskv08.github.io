8ca1365f57f8536d0b1d958760e73bca24d6ba00
robbmoskv <fabrizio_39@hotmail.com>
2022-08-05 11:51:13 +0200

    Update config file t include base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 // vite.config.js
 import vitePluginString from 'vite-plugin-string'
+import { defineConfig } from 'vite'
 
 export default {
   plugins: [
-    vitePluginString.default()
+    vitePluginString.default(),
+    defineConfig({
+      base: "./"
+    })   
   ]
 }
