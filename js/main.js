c4b8862d29dc14e4dd750196a5c0fdb4582381cf
robbmoskv <fabrizio_39@hotmail.com>
2022-08-08 10:51:47 +0200

    Clean up

--- js/main.js
+++ js/main.js
@@ -29,8 +29,6 @@ function render(){
 }
 
 function resizeCanvas(){
-  // canvas.width = window.innerWidth;
-  // canvas.height = window.innerHeight;
   canvas.style.width = '100%';
   canvas.style.height = '100%';
 
